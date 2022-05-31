048baac841ef7e5c4a2c1ee9a96401f9867ab3a3
Przemek-Szadkowski <przemoszadkowski@o2.pl>
2022-05-31 20:32:46 +0200

    Add router and view when bike is not foun

---
+++ routers/bike.router.ts
@@ -0,0 +1,26 @@
+import {Request, Response, Router} from "express";
+import {BikeRecord} from "../records/bike.record";
+import {MessageRecord} from "../records/message.record";
+
+export const bikeRouter = Router()
+
+    .get('/:orderNo', async (req: Request, res: Response) => {
+        const bike = await BikeRecord.getOneByOrderNo(req.params.orderNo);
+        res.json(bike);
+    })
+
+    .post('/:orderNo', async (req: Request, res: Response) => {
+        const bike = await BikeRecord.getOneByOrderNo(req.body.orderNo);
+
+        const newMessage = new MessageRecord({
+            text: req.body.textAreaVal,
+            isClientAsk: req.body.isClientAsk,
+            isNew: req.body.isNew,
+        });
+
+        await newMessage.insertMessage(bike.id);
+
+        const messages = await MessageRecord.getMessagesById(bike.id);
+
+        res.json(messages);
+    })
