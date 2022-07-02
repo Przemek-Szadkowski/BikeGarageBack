de273bc1bb83514a30d58a02c016cb15f945c1a9
Przemek-Szadkowski <przemoszadkowski@o2.pl>
2022-07-02 13:40:09 +0200

    Added config example and config.ts and moved dbConnection data into config

--- utils/db.ts
+++ utils/db.ts
@@ -1,11 +1,17 @@
 import {createPool} from "mysql2/promise";
+import {config} from "../config/config";
 
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'bike_garage',
+    host: config.dbHost,
+    user: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbDatabase,
     namedPlaceholders: true,
     decimalNumbers: true,
     timezone: '+0100',
 })
 
+// Baza danych:	bikegara_api
+// Host:	localhost
+// Nazwa użytkownika:	bikegara_api
+// Hasło:	C5cCMs4k
